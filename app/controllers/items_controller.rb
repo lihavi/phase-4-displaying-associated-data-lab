c97a1c81590dc388366c3d1557a6510100a2115c
vivianne <viviannjoroge91@gmail.com>
2023-03-20 08:39:00 +0300

    done

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,18 @@
 class ItemsController < ApplicationController
+    def index
+        items = Item.all.map do |item|
+          {
+            id: item.id,
+            name: item.name,
+            description: item.description,
+            price: item.price,
+            user: {
+              id: item.user.id,
+              username: item.user.username,
+              city: item.user.city
+            }
+          }
+        end
+        render json: items
+      end
 end
